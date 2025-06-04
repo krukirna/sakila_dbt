8a087a0bdc24b7f8176ca85296983bb11f354d92
krukirna <brisjony@gmail.com>
2025-06-04 18:07:17 +0300

    macro 2nd

--- models/example/macro_models.sql
+++ models/example/macro_models.sql
@@ -1,3 +1,4 @@
 select
-{{ macro_example('title', 'description') }} as macro_example_output
+    {{ macro_example('title', 'description') }} as macro_example_output,
+    {{ clean_string('description') }} as cleaned_description
 from {{source('stg', 'film') }}
