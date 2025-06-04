07b0b3bf70f78e17e177e1cf6c976f74ea32be99
krukirna <brisjony@gmail.com>
2025-06-04 19:28:40 +0300

    utils 3

---
+++ models/example/dim_date_star_exclude.sql
@@ -0,0 +1,6 @@
+select 
+{{ dbt_utils.star(from=ref('dim_date'), except=['weekend_indr', 'date_dim_id', 'month_name_abbreviated', 'epoch'], relation_alias = 'dim_date' ) }},
+{{ dbt_utils.star(from=ref('dim_customer'), relation_alias = 'cust', suffix = '_cust', quote_identifiers=false ) }}
+from {{ ref('dim_date') }} as dim_date
+right join {{ ref('dim_customer') }} as cust on cust.create_date = dim_date.date_key
+where dim_date.date_key is not null
