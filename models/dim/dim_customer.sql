9c8d18f82d857c557a66856488ba82e11a09504d
krukirna <brisjony@gmail.com>
2025-06-04 20:42:05 +0300

    hook 1

--- models/dim/dim_customer.sql
+++ models/dim/dim_customer.sql
@@ -1,9 +1,15 @@
 {{
 	  config(
 		unique_key = 'customer_id',
+		post_hook = "update {{source('stg', 'z_refresh_from')}} set to_refresh = 0 where table_name = '{{ this }}'"
 		)
 }}
 
+with refresh_date as (
+	select from_date
+	from {{ source('stg','z_refresh_from') }}
+	where table_name = '{{this}}' and to_refresh = 1
+)
 select  ci.city as city, 
 		c.first_name || ' ' || c.last_name as full_name,
 		right(c.email, length(c.email) - position('@' in c.email)) as domain,
@@ -15,5 +21,9 @@ left join {{ source('stg','city') }} as ci on ci.city_id = a.city_id
 
 
 {% if is_incremental() %}
-	where c.last_update >= coalesce((select max(last_update) from {{ this }}), '1900-01-01')
+--	where c.last_update >= coalesce((select max(last_update) from {{ this }}), '1900-01-01')
+	where c.last_update >= coalesce(
+									(select from_date from refresh_date ), 
+									(select max(last_update) from {{ this }}),
+									'1900-01-01')
 {% endif %}
